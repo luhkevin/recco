f4ee4086929038917b8d8ef82de54f4770df7152
Fan Yin <fanyin1234@gmail.com>
2013-09-07 14:10:07 -0400

    friends page

--- friends/views.py
+++ friends/views.py
@@ -1,1 +1,31 @@
 # Create your views here.
+
+from django.shortcuts import render
+from friends.models import Friendship
+from django.contrib.auth.models import User
+
+def index(request):
+    user = request.user.username
+    
+    if request.POST:
+        newf = request.POST.get("new")
+        lookup = User.objects.filter(username = newf)
+        
+        if len(lookup) == 0:
+            fr = Friendship.objects.filter(source__user__username__exact = user)
+            friends = map(lambda f: f.target, fr)
+            return render(request, 'friends/index.html', {'error': 'that is not a valid user', 'friends': friends}) 
+        
+        else:
+            f = Friendship(source = user, target = newf)
+            f.save()
+            fr = Friendship.objects.filter(source__user__username__exact = user)
+            friends = map(lambda f: f.target, fr)
+            return render(request, 'friends/index.html', {'error': 'successful!', 'friends': friends})
+    
+    else:
+        fr = Friendship.objects.filter(source__user__username__exact = user)
+        friends = map(lambda f: f.target, fr)
+        return reder(request, 'friends/index.html', {'error':'', 'friends':friends})
+
+
--- recco/urls.py
+++ recco/urls.py
@@ -13,7 +13,8 @@ urlpatterns = patterns('',
     url(r'^home/', 'startpage.views.homepage'),
 
     url(r'^mymedia/', 'MyMedia.views.index'),
-
+    
+    url(r'^friends/', 'friends.views.index'),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
