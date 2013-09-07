047ee2ee0c19f1e5425f6923630bf288755ec2f9
Fan Yin <fanyin1234@gmail.com>
2013-09-07 00:50:16 -0400

    mymedia views

--- MyMedia/views.py
+++ MyMedia/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+
+from django.shortcuts import render_to_response
+from models import Media
+
+def index(request):
+  recs = Media.objects.order_by('name')
+  return render_to_response('MyMedia/index.html', recs)
