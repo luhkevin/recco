64e400de1bab085f3ef42f3c5f0b6b46eb704ded
Fan Yin <fanyin1234@gmail.com>
2013-09-07 01:12:33 -0400

    mymedia html page

--- MyMedia/views.py
+++ MyMedia/views.py
@@ -4,5 +4,13 @@ from django.shortcuts import render_to_response
 from models import Media
 
 def index(request):
+  if request.POST:
+    newrec = request.POST.get("new")
+
+    r = Media(name = newrec)
+    r.save()
+  
   recs = Media.objects.order_by('name')
   return render_to_response('MyMedia/index.html', recs)
+
+
