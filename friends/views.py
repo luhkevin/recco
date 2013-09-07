3e6bf320f81b3cd83ae77cbbbb546b4dd2ed98a2
Kevin Lu <luhkevin@gmail.com>
2013-09-07 18:38:45 -0400

    linked profiles

--- friends/views.py
+++ friends/views.py
@@ -6,16 +6,16 @@ from django.contrib.auth.models import User
 
 def index(request):
     user = request.user.username
-    
+
     if request.POST:
         newf = request.POST.get("new")
         lookup = User.objects.filter(username = newf)
-        
+
         if len(lookup) == 0:
             fr = Friendship.objects.filter(source__user__username__exact = user)
             friends = map(lambda f: f.target, fr)
-            return render(request, 'friends/index.html', {'error': 'that is not a valid user', 'friends': friends}) 
-        
+            return render(request, 'friends/index.html', {'error': 'that is not a valid user', 'friends': friends})
+
         else:
             u = User.objects.get(username = user).person
             t = lookup[0].person
@@ -26,7 +26,7 @@ def index(request):
             fr = Friendship.objects.filter(source__user__username__exact = user)
             friends = map(lambda f: f.target, fr)
             return render(request, 'friends/index.html', {'error': 'successful!', 'friends': friends})
-    
+
     else:
         fr = Friendship.objects.filter(source__user__username__exact = user)
         friends = map(lambda f: f.target, fr)
