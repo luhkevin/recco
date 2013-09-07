95cb4bd4a7b889414b12f1f4b14487a67e0a23f3
Yukuan Zhang <yukuan@sas.upenn.edu>
2013-09-06 22:29:38 -0400

    basic authentication works

---
+++ auth/views.py
@@ -0,0 +1,25 @@
+# Create your views here.
+
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+from django.contrib.auth import authenticate, login
+
+def login_user(request):
+    state = "Please log in:"
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "Successfully logged in."
+            else: 
+                state = "Account inactive."
+        else:
+            state = "User/pass incorrect."
+    c = {'state':state, 'username':username};
+    c.update(csrf(request))
+    return render_to_response('auth.html',c)
