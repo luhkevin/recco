3032ebbc3a506aae67a29621109cd10cfb50c29c
Kevin Lu <luhkevin@gmail.com>
2013-09-07 14:12:41 -0400

    no repeated things

--- MyMedia/views.py
+++ MyMedia/views.py
@@ -6,7 +6,8 @@ from MyMedia.models import Media
 def index(request):
   if request.POST:
     newrec = request.POST.get("new")
-    r = Media(name = newrec)
-    r.save()
+    if not Media.objects.filter(name__contains = newrec).count() > 1:
+        r = Media(name = newrec)
+        r.save()
   recs = Media.objects.order_by('name')
   return render(request, 'MyMedia/index.html', {'recs': recs})
