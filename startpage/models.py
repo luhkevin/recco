a1a7c0d278184ab2dc018d425dead2ba58d09684
Yukuan Zhang <yukuan@sas.upenn.edu>
2013-09-07 03:52:31 -0400

    all models should be good

--- startpage/models.py
+++ startpage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from MyMedia.models import *
+from friends.models import *
 
 # Create your models here.
+
+class Person(models.Model):
+    realname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+
+    completed = models.ManyToManyField(Media, through='Completed')
+    friends = models.ManyToManyField('self', through='Friendship', symmetrical=False)
+    
+
+    def __unicode__(self):
+        return self.username
