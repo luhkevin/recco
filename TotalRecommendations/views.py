29473b243e9dc00eed455f73417bb36ce5432ea8
Kevin Lu <luhkevin@gmail.com>
2013-09-07 15:13:34 -0400

    totalrecs

--- TotalRecommendations/views.py
+++ TotalRecommendations/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+
+from django.shortcuts import render
+from MyMedia.models import Recommendation, Media
+
+def index(request):
+    #total_recs = Recommendation.objects.orderby('time')
+
+    #this is filler
+    total_recs = Media.objects.order_by('name')
+
+    return render(request, 'TotalRecommendations/index.html', {'total_recs': total_recs})
