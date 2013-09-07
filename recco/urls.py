413488b1359142512273b5474c4957ad4ad45b8c
Yukuan Zhang <yukuan@sas.upenn.edu>
2013-09-06 22:06:11 -0400

    trying auth

--- recco/urls.py
+++ recco/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'recco.views.home', name='home'),
     # url(r'^recco/', include('recco.foo.urls')),
+    url(r'^login/$', 'auth.views.login_user'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
