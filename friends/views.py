e7ee071e90440e5481b4a385a164b7a9dfcef1a9
Fan Yin <fanyin1234@gmail.com>
2013-09-08 02:42:01 -0400

    linked username and points

--- friends/views.py
+++ friends/views.py
@@ -5,6 +5,7 @@ from friends.models import Friendship
 from django.contrib.auth.models import User
 
 def index(request):
+    userperson = request.user.person
     user = request.user.username
 
     if request.POST:
@@ -12,29 +13,29 @@ def index(request):
         lookup = User.objects.filter(username = newf)
 
         if len(lookup) == 0 or user == newf:
-            fr = Friendship.objects.filter(source__user__username__exact = user)
+            fr = Friendship.objects.filter(source = userperson)
             friends = map(lambda f: f.target, fr)
-            return render(request, 'friends/index.html', {'error': 'that is not a valid user', 'friends': friends})
+            return render(request, 'friends/index.html', {'error': 'that is not a valid user', 'friends': friends, 'username': userperson.firstname, 'points': userperson.currentpoints})
 
         else:
-            u = User.objects.get(username = user).person
+            u = request.user
             t = lookup[0].person
             friends = Friendship.objects.filter(source = u, target = t)
             if len(friends) > 0:
-                fr = Friendship.objects.filter(source__user__username__exact = user)
+                fr = Friendship.objects.filter(source = userperson)
                 friends = map(lambda f: f.target, fr)
-                return render(request, 'friends/index.html', {'error': 'you are already friends', 'friends': friends})
+                return render(request, 'friends/index.html', {'error': 'you are already friends', 'friends': friends, 'username': userperson.firstname, 'points': userperson.currentpoints})
             f = Friendship(source = u, target = t)
             f2 = Friendship(source = t, target = u)
             f.save()
             f2.save()
-            fr = Friendship.objects.filter(source__user__username__exact = user)
+            fr = Friendship.objects.filter(source = userperson)
             friends = map(lambda f: f.target, fr)
-            return render(request, 'friends/index.html', {'error': 'successful!', 'friends': friends})
+            return render(request, 'friends/index.html', {'error': 'successful!', 'friends': friends, 'username': userperson.firstname, 'points': userperson.currentpoints})
 
     else:
-        fr = Friendship.objects.filter(source__user__username__exact = user)
+        fr = Friendship.objects.filter(source = userperson)
         friends = map(lambda f: f.target, fr)
-        return render(request, 'friends/index.html', {'error':'', 'friends':friends})
+        return render(request, 'friends/index.html', {'friends':friends, 'username': userperson.firstname, 'points': userperson.currentpoints})
 
 
