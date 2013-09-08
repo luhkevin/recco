a1858e88f4383f8a28bc92b10843c35bec890671
Yukuan Zhang <yukuan@sas.upenn.edu>
2013-09-07 21:17:58 -0400

    private mymedia

--- MyMedia/views.py
+++ MyMedia/views.py
@@ -3,11 +3,13 @@
 from django.shortcuts import render
 from MyMedia.models import Media
 
-def index(request):
-  if request.POST:
-    newrec = request.POST.get("new")
-    if not Media.objects.filter(name = newrec).count() == 1:
-        r = Media(name = newrec)
-        r.save()
-  recs = Media.objects.order_by('name')
-  return render(request, 'MyMedia/index.html', {'recs': recs})
+def index(request)
+    userperson = request.user.person
+    
+    if request.POST:
+      newrec = request.POST.get("new")
+      if not Media.objects.filter(name = newrec).count() == 1:
+          r = Media(name = newrec)
+          r.save()
+    recs = userperson.completedmedia.objects.order_by('name')
+    return render(request, 'MyMedia/index.html', {'recs': recs})
